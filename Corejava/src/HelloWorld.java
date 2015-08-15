dc3fd298373c993311f64f1fdc702f36359a49a4
Nithuna <Nithuna@Nithuna-R>
2015-08-15 09:24:47 -0400

    Date Aug 11

--- Corejava/src/HelloWorld.java
+++ Corejava/src/HelloWorld.java
@@ -4,9 +4,12 @@ public class HelloWorld {
 	{
 		int i=0;
 		for(i=0;i<5;i++)
+		{
 		System.out.println("I am printing Argument" + i  + " " + args[i]);
 		String output = ( i > 3)? "Greater" : " Smaller" ;
 		System.out.println(output);
+		}
+		
 	}
 
 }
---
+++ Corejava/src/datatypes/IntExample/IntExample.java
@@ -0,0 +1,16 @@
+package datatypes.IntExample;
+
+public class IntExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int one = 20;
+		int two = 30;
+		byte three = 40;
+		char ch = 'A';
+		System.out.println(one+two+three+ch);
+				
+
+	}
+
+}
