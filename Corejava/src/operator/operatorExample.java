8ea7984f70520090d10931dda844e9d04dfb23c0
Nithuna <Nithuna@Nithuna-R>
2015-08-15 21:30:52 -0400

    Updated

---
+++ Corejava/src/operator/operatorExample.java
@@ -0,0 +1,20 @@
+package operator;
+
+public class operatorExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a =23;
+		int b = 110;
+		System.out.println(++a);
+		System.out.println(--b);
+	    a +=2;
+	    System.out.println(a);
+	    a *=4;
+	    System.out.println(a);
+		
+	
+
+	}
+
+}
---
+++ Corejava/src/operator/relationaloperator.java
@@ -0,0 +1,17 @@
+package operator;
+
+public class relationaloperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String a = "100";
+		String b = "200";
+		
+				
+		System.out.println(a == b );
+		System.out.println(a != b );
+		//System.out.println(a + "==" + b + "==" + (a > b);
+	}
+
+}
